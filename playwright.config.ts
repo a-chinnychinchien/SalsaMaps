3c2b8f9425ca1c2c473b6c5b93b892f10f530ddd
Alan Chien <chien.alan.dev@gmail.com>
2025-10-09 16:54:33 -0400

    Minor cleanup of playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from '@playwright/test';
 import 'dotenv/config';
 
+const serverAddress=`http://localhost:${process.env.PORT}`;
+
 export default defineConfig({
-  // Base URL is crucial for API tests
+  testDir: './tests', // Location of your test files
+
   use: {
     // Playwright will automatically prefix all test calls with this URL
-    baseURL: `http://localhost:${process.env.PORT}`, 
+    baseURL: serverAddress, 
     trace: 'on-first-retry',
   },
-  testDir: './tests', // Location of your test files
-  // ... other standard config options
+
+  // TODO: Make this work
+  // // Automatically start server before running tests
+  // webServer: {
+  //   command: 'npm run start:dev', 
+  //   url: serverAddress,
+  //   timeout: 120 * 1000, 
+  //   reuseExistingServer: false,
+  // },
 });
 
 //Run tests via: `npx playwright test`

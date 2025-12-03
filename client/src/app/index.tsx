e187b77029117249a68168d16b9870d6abb3f30f
Alan Chien <chien.alan.dev@gmail.com>
2025-12-03 10:44:15 -0500

    Switch to expo-maps. Android Google Maps still not displaying, continue with iOS development and plan to fix later

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,20 +1,34 @@
-import { Text, StyleSheet, View } from "react-native";
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import { AppleMaps, GoogleMaps } from 'expo-maps';
+import { Platform, Text } from 'react-native';
 
-export default function Index() {
-  return (
-    <View>
-      <MapView provider={PROVIDER_GOOGLE} style={styles.map}/>
-    </View>
-  );
+export default function App() {
+  if (Platform.OS === 'ios') {
+    return <AppleMaps.View style={{ flex: 1 }} />;
+  } else if (Platform.OS === 'android') {
+    return <GoogleMaps.View style={{ flex: 1 }} />;
+  } else {
+    return <Text>Maps are only available on Android and iOS</Text>;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  map: {
-    height: '50%',
-    width: '50%',
-  }
-});
+// import { Text, StyleSheet, View } from "react-native";
+// import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+
+// export default function Index() {
+//   return (
+//     <View>
+//       {/* <MapView provider={PROVIDER_GOOGLE} style={styles.map}/> */}
+//       <MapView  style={styles.map}/>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1
+//   },
+//   map: {
+//     height: '75%',
+//     width: '100%',
+//   }
+// });

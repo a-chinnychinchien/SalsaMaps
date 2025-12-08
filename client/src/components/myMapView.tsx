00b07e1bed044d80b64a3e45c6118ec60c1cdafc
Alan Chien <chien.alan.dev@gmail.com>
2025-12-08 16:41:22 -0500

    Populate Event markers from dummy data. Requires geocoding to get lat, long coordinates

--- client/src/components/myMapView.tsx
+++ client/src/components/myMapView.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Platform, Text, View, Button, Modal, Pressable } from 'react-native';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { IOS, ANDROID } from '../constants/constants';
 import * as Location from 'expo-location';
 import { TrueSheet } from "@lodev09/react-native-true-sheet"
@@ -7,9 +7,47 @@ import { SalsaEvent, currEvents } from '../testing_data/events'
 import { AppleMaps, GoogleMaps } from 'expo-maps';
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
 
+type AppleMapMarker = {
+  coordinates: {latitude: number, longitude: number}
+  title: string
+  id: string
+  systemImage?: string
+  tintColor?: string
+}
 
+async function getCurrMarkers(): Promise<AppleMapMarker[]> {
+  let currMarkers:AppleMapMarker[] = [{
+          coordinates: { latitude: 40.68666348121151, longitude: -73.9676898608171 },
+          title: 'Initial',
+          id: '-1',
+        }]
+  
+  for (let i=0;i<currEvents.length;i++) {
+    const currEvent = currEvents[i];
+    // Geocode the address
+    const geocode = await Location.geocodeAsync(currEvent.address);
+    currMarkers.push({
+      coordinates: {latitude: geocode[0].latitude, longitude: geocode[0].longitude},
+      id: String(i),
+      title: `Event${i}`
+    })
+  }
+
+  return currMarkers
+} 
 
 export default function MyMapView(): React.JSX.Element {
+  // Obtain markers
+  let [markers, setMarkers] = useState<AppleMapMarker[]>([])
+  useEffect(()=>{
+    async function loadMarkers(){
+      const markers = await getCurrMarkers();
+      setMarkers(markers);
+    }
+    loadMarkers();
+  },[]);
+
+  // Bottom Sheet setup
   const sheet = useRef<TrueSheet>(null);
   const present = async () => {
     await sheet.current?.present()
@@ -24,15 +62,11 @@ export default function MyMapView(): React.JSX.Element {
     return <>
       <AppleMaps.View
         style={{ flex: 1 }}
-        markers={[{
-          coordinates: { latitude: 40.68666348121151, longitude: -73.9676898608171 },
-          title: 'Initial',
-          id: '0'
-        }]}
+        markers={markers}
         onMarkerClick={present}
         cameraPosition={{
           coordinates: { latitude: 40.68666348121151, longitude: -73.9676898608171 },
-          zoom: 14
+          zoom: 12
         }}
       />
       <TrueSheet

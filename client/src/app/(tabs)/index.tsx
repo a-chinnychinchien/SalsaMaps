4ff21b347eecce709a6e9ca8c595dbba2aeb75b7
Alan Chien <chien.alan.dev@gmail.com>
2025-12-09 11:31:43 -0500

    Check for and request location permissions if needed before initializing map.

--- client/src/app/(tabs)/index.tsx
+++ client/src/app/(tabs)/index.tsx
@@ -3,58 +3,43 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { useEffect, useState } from 'react'
 import * as Location from 'expo-location'
 import { Text } from 'react-native'
+import { DEFAULT_INIT_LOC } from '../../constants/constants';
 
 export default function App() {
   let [initialLoc, setInitialLoc] = useState<Location.LocationObject | null>(null)
   useEffect(() => {
     async function getCurrLoc() {
-      try {
-        const initialLoc = await Location.getCurrentPositionAsync();
-        setInitialLoc(initialLoc);
+      // Request permissions if needed
+      let locPermissions = await Location.getForegroundPermissionsAsync()
+      if (!locPermissions.granted) {
+        locPermissions = await Location.requestForegroundPermissionsAsync()
       }
-      catch (error) {
-        console.log(error)
-        // Fallback
-        setInitialLoc({
-          coords: { altitude: null, accuracy: null, altitudeAccuracy: null, heading: 0, speed: 0, latitude: 40.74619587808718, longitude: -73.92451420314279 },
-          timestamp: Date.now()
-        })
+
+      // Get current location or use default
+      let initialLoc = DEFAULT_INIT_LOC;
+      if (locPermissions.granted) {
+        try {
+          initialLoc = await Location.getCurrentPositionAsync();
+        }
+        catch (error) {
+          console.log(`Error obtaining current position: ${error}`)
+          console.log('Defaulting to Times Square as initial location.')
+        }
       }
+      else {
+        console.log('Location permissions not granted.')
+        console.log('Defaulting to Times Square as initial location.')
+      }
+      setInitialLoc(initialLoc);
     }
+
+    // run the async fxn
     getCurrLoc();
   }, [])
 
   if (initialLoc === null) {
-    // Return a loading state or a blank screen until data is ready
     return <Text>Loading Location...</Text>;
   }
 
   return <MyMapView initialLoc={initialLoc} />;
 }
-
-
-//
-// react-native-maps try
-//
-
-// import { Text, StyleSheet, View } from "react-native";
-// import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-
-// export default function Index() {
-//   return (
-//     <View>
-//       {/* <MapView provider={PROVIDER_GOOGLE} style={styles.map}/> */}
-//       <MapView  style={styles.map}/>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1
-//   },
-//   map: {
-//     height: '75%',
-//     width: '100%',
-//   }
-// });
--- client/src/constants/constants.tsx
+++ client/src/constants/constants.tsx
@@ -1,2 +1,17 @@
+import * as Location from 'expo-location'
+
 export const IOS = 'ios';
 export const ANDROID = 'android';
+
+export const DEFAULT_INIT_LOC: Location.LocationObject = {
+  coords: {
+    latitude: 40.75810470031284,
+    longitude: -73.98555333197822,
+    accuracy: null,
+    altitude: null,
+    altitudeAccuracy: null,
+    heading: null,
+    speed: null
+  },
+  timestamp: Date.now()
+}
